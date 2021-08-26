60434f6668d69f1834682c0df7a9b4d528856726
Peter Van Dyken <pvandyk2@uwo.ca>
2021-08-26 11:39:01 -0400

    New multi-shell testing function

--- workflow/lib/shells.py
+++ workflow/lib/shells.py
@@ -4,7 +4,6 @@ def is_multi_shelled(bval_file: str):
     with open(bval_file) as bval_s:
         bvals = filter(None, tz.first(bval_s).split(" "))
         ranks = tz.groupby(_shell_rank, bvals)
-        print(ranks)
         return len(ranks) > 2
 
         
