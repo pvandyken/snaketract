ebf900b69e85f0b0a96504554f967d4312ceb022
Peter Van Dyken <pvandyk2@uwo.ca>
2021-08-26 11:38:32 -0400

    Uses command line arguments

---
+++ run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+from snakebids.app import SnakeBidsApp
+
+
+
+def main():
+    app = SnakeBidsApp(os.path.abspath(os.path.dirname(__file__)))
+    app.config["output_dir"] = os.path.join(app.config["output_dir"], "prepdwi_recon")
+    app.run_snakemake()
+
+
+if __name__ == "__main__":
+    main()
