e267540bdec725aefc50bfeec7eafbe61a4b1d9e
Peter Van Dyken <pvandyk2@uwo.ca>
2022-02-07 19:33:53 -0500

    Remove unneccessary args and fix var names

--- workflow/scripts/produce-r1.py
+++ workflow/scripts/produce-r1.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
     args = snakemake_args()
     assert isinstance(args.input, dict)
     assert isinstance(args.output, list)
-    assert isinstance(args.wildcards, dict)
 
-    data = args.input["data"]
-    mask = args.input["mask"]
+    data_path = args.input["data"]
+    mask_path = args.input["mask"]
     output = Path(args.output[0])
 
-    img = nib.load(data)
-    mask_data = nib.load(t1_mask_path).get_fdata().astype(bool)
+    t1map = nib.load(data_path)
+    data = t1map.get_fdata()
+    mask_data = nib.load(mask_path).get_fdata().astype(bool)
     masked = np.ma.masked_where(~mask_data, data)
     r1_data = (1/masked).data
     r1_data[~mask_data] = 0
 
-    r1 = nib.Nifti1Image(r1, t1map.affine)
+    r1 = nib.Nifti1Image(r1_data, t1map.affine)
     nib.save(r1, output)
